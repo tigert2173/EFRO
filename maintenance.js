b42751ab8e9b08df55b13ea1712fc62e1c8fb258
tigert2173 <130621909+tigert2173@users.noreply.github.com>
2024-09-10 19:23:51 -0400

    Update maintenance.js

--- maintenance.js
+++ maintenance.js
@@ -4,12 +4,21 @@ var isMaintenanceMode = true; // Change this to true to enable maintenance mode
 // Function to handle maintenance mode
 function checkMaintenanceMode() {
     if (isMaintenanceMode) {
+        document.getElementById('overlay').style.display = 'block';
         document.getElementById('maintenanceMessage').style.display = 'block';
         document.getElementById('content').style.display = 'none';
     } else {
+        document.getElementById('overlay').style.display = 'none';
         document.getElementById('maintenanceMessage').style.display = 'none';
         document.getElementById('content').style.display = 'block';
     }
+}
+
+// Function to close maintenance message
+function closeMaintenance() {
+    document.getElementById('overlay').style.display = 'none';
+    document.getElementById('maintenanceMessage').style.display = 'none';
+    document.getElementById('content').style.display = 'block';
 }
 
 // Check maintenance mode on page load
